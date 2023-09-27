393783aa404b92ff127e4195a82e5e812e3fa30c
Manoj G <102495616+manojmanu276@users.noreply.github.com>
2023-09-27 15:35:15 +0530

    shellscript_tofind_10biggestfiles_in_linuxsystem

--- 10_biggest_files_linux.sh
+++ 10_biggest_files_linux.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-find / -type f printf "%s %p\n" | sort -rn | head -n 10 > 10biggest.txt
+find / -type f printf "%s %p\n" | sort -rn | head -n 10 > 10biggestfiles.txt
+
+<<com
+-> #!/bin/bash specifies the shell to use for running the script.
+-> find / -type f -printf '%s %p\n' searches for all files in the root directory / and prints their size and path, separated by a space, with a newline character at the end of each line.
+-> sort -nr sorts the output of find numerically in reverse order, so the largest files are listed first.
+-> head -10 shows only the first 10 lines of output, which correspond to the 10 largest files in the system.
+-> > 10biggestfiles.txt redirects the output of the entire pipeline to a file named 10biggestfiles.txt in the current working directory.
+com
