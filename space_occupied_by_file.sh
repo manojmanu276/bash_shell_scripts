7343bf600bd9ce26f082918c19aaffbba76d0866
Cloud User <ec2-user@ip-172-31-13-140.ap-south-1.compute.internal>
2023-10-09 11:55:00 +0000

    script to check space utilised by a certain file

---
+++ space_occupied_by_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the target filesystem you want to check
+target_fs="jenkins_install.yaml"
+
+# Use the 'df' command to retrieve disk usage information and filter by the target filesystem
+usage_info=$(du -sh "$target_fs")
+
+# Check if the target filesystem exists and display the usage information
+if [ $? -eq 0 ]; then
+  echo "Disk usage information for $target_fs:"
+  echo "$usage_info"
+else
+  echo "Error: The specified filesystem '$target_fs' does not exist or could not be accessed."
+fi
